5ff77c4c56287998dd4f30d92ba324314331ff10
PiciMadar <rekdavid@turr.hu>
2025-01-17 10:33:00 +0100

    Megpróbáltam az insertet, sírva fakadtam 🙌🌹💋

--- IdojarasTEST/Januar.cs
+++ IdojarasTEST/Januar.cs
@@ -8,11 +8,14 @@ namespace IdojarasSQL
 {
     internal class Januar
     {
-        public int Id;
-        public int Datum;
-        public int Homerseklet;
-        public int Csapadek;
-        public int Parataltalom;
+        public int Id { get; set; }
+        public int Datum { get; set; }
+        public int Homerseklet { get; set; }
+        public int Csapadek { get; set; }
+        public int Parataltalom { get; set; }
+
+
+
 
         public Januar(int id, int datum, int homerseklet, int csapadek, int parataltalom)
         {
@@ -21,6 +24,21 @@ namespace IdojarasSQL
             Homerseklet = homerseklet;
             Csapadek = csapadek;
             Parataltalom = parataltalom;
+        }
+
+        public override string ToString() 
+        {
+            return $"--------------------------\nID = {Id}\nDátum = {Datum}\nHőmérséklet = {Homerseklet}\nCsapadék = {Csapadek}\nPáratartalom = {Parataltalom}\n--------------------------";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Januar januar &&
+                   Id == januar.Id &&
+                   Datum == januar.Datum &&
+                   Homerseklet == januar.Homerseklet &&
+                   Csapadek == januar.Csapadek &&
+                   Parataltalom == januar.Parataltalom;
         }
     }
 }
--- IdojarasTEST/Program.cs
+++ IdojarasTEST/Program.cs
@@ -16,6 +16,84 @@ namespace IdojarasSQL
         static void Main(string[] args)
         {
             KapcsolodasAdatbazishoz();
+            SelectTable("idojaras");
+            for (int i = 0; i < januarList.Count; i++) 
+            {
+                Console.WriteLine(januarList[i].ToString());
+            }
+            Beolvasas();
+            UjSor();
+        }
+
+        private static void UjSor()
+        {
+            try
+            {
+                using (MySqlCommand commandInsert = new MySqlCommand($"insert into idojaras (id, datum, homerseklet, csapadek, paratartalom) values (@id, @datum, @homerseklet, @csapadek, @paratartalom)", connection))
+                {
+                    commandInsert.Parameters.AddWithValue("@id", januarList[januarList.Count() -1].Id);
+                    commandInsert.Parameters.AddWithValue("@datum", januarList[januarList.Count() - 1].Datum);
+                    commandInsert.Parameters.AddWithValue("@homerseklet", januarList[januarList.Count() - 1].Homerseklet);
+                    commandInsert.Parameters.AddWithValue("@csapadek", januarList[januarList.Count() - 1].Csapadek);
+                    commandInsert.Parameters.AddWithValue("@paratartalom", januarList[januarList.Count() - 1].Parataltalom);
+                    commandInsert.ExecuteNonQuery();
+
+                    Console.WriteLine("Sikeres INSERT!");
+
+                    //Lista újratöltés...
+                    SelectTable("idojaras");
+                    
+                }
+            }
+            catch (MySqlException mySqlError)
+            {
+                Console.WriteLine("Sikertelen INSERT");
+                Console.WriteLine(mySqlError);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Sikertelen INSERT");
+                Console.WriteLine(error);
+            }
+        }
+
+        private static void Beolvasas()
+        {
+            string FilePath = "SQL.txt";
+            if (File.Exists(FilePath))
+            {
+                string[] sorok = File.ReadAllLines(FilePath);
+
+                for (int i = 0;i < sorok.Length; i++)
+                {
+                    string[] mezok = sorok[i].Split(';');
+                    Januar UjDatom = new Januar(Convert.ToInt32(mezok[0]), Convert.ToInt32(mezok[1]), Convert.ToInt32(mezok[2]), Convert.ToInt32(mezok[3]), Convert.ToInt32(mezok[4]));
+                    januarList.Add(UjDatom);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valami nem stimmel");
+            }
+            Console.WriteLine(januarList[januarList.Count()-1].ToString());
+        }
+
+        private static void SelectTable(string v)
+        {
+            using (MySqlCommand command = new MySqlCommand($"select * from {v}", connection))
+            {
+                januarList.Clear();
+                using (MySqlDataReader mySqlDataReader = command.ExecuteReader())
+                {
+                    while (mySqlDataReader.Read())
+                    {
+                        Januar januarTemp = new Januar(Convert.ToInt32(mySqlDataReader[0]), Convert.ToInt32(mySqlDataReader[1]),
+                                                       Convert.ToInt32(mySqlDataReader[2]), Convert.ToInt32(mySqlDataReader[3]),
+                                                       Convert.ToInt32(mySqlDataReader[4]));
+                        januarList.Add(januarTemp);
+                    }
+                }
+            }
         }
 
         private static void KapcsolodasAdatbazishoz()
