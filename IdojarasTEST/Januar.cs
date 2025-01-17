beb066a8b81abdc80a87e9b143864cb42ae0cb12
PiciMadar <rekdavid@turr.hu>
2025-01-17 09:36:15 +0100

    Add project files.

---
+++ IdojarasTEST/Januar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdojarasSQL
+{
+    internal class Januar
+    {
+        public int Id
+        public int Datum
+        public int Homerseklet
+        public int Csapadek
+        public int Parataltalom
+
+        public Januar(int id, int datum, int homerseklet, int csapadek, int parataltalom)
+        {
+            Id = id;
+            Datum = datum;
+            Homerseklet = homerseklet;
+            Csapadek = csapadek;
+            Parataltalom = parataltalom;
+        }
+    }
+}
---
+++ IdojarasTEST/Program.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace IdojarasSQL
+{
+    internal class Program
+    {
+        private static string server = "localhost";
+        private static string database = "idojaras3";
+        private static string user = "root";
+        private static string password = "";
+        private static List<Januar> januarList = new List<Januar>();
+
+
+
+    }
+}
